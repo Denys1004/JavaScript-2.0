3fc630fd9d5165dbdbf64bcb305fc1990407c02d
denys krychevskyi <dkrychevskyi@gmail.com>
2020-11-18 11:50:08 -0800

    added uniit 20

---
+++ UNIT 20. СОБЫТИЯ КЛАВИАТУРЫ В JAVASCRIPT/app/script.js
@@ -0,0 +1,36 @@
+    document.querySelector('.i-1').onkeypress = function (event) {
+        console.log('keypress');
+        console.log('charCode: ' + event.charCode); //q 113 Q 81
+        console.log('code: ' + event.code);
+        console.log('key: ' + event.key);
+        console.log('keyCode: ' + event.keyCode);
+        // console.log(event);
+    }
+
+    document.querySelector('.i-1').onkeydown = function (event) {
+        console.log('key: ' + event.key);
+        if (event.key == 'CapsLock') {
+            document.querySelector('.ch-1').checked = true;
+        }
+        else {
+            document.querySelector('.ch-1').checked = false;
+        }
+    }
+
+document.querySelector('.i-1').onkeyup = function (event) {
+    console.log('keyup');
+    console.log('charCode: ' + event.charCode); //q 113 Q 81
+    console.log('code: ' + event.code);
+    console.log('key: ' + event.key);
+    console.log('keyCode: ' + event.keyCode);
+}
+
+    document.querySelector('.i-2').onkeypress = function (event) {
+        console.log('key: ' + event.key);
+        const a = {
+            q: 'w',
+            w: 'e'
+        }
+        document.querySelector('.i-2').value += a[event.key];
+        return false;
+    }
